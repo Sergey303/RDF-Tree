3e3546d41bbb3e43b16700e3087179588433f4d6
Sergey303 <s@s.s>
2016-01-06 17:53:11 +0600

    Start

---
+++ BinaryTable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BinaryTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = XElement.Load("../../../tree.xml");
+            Dictionary<string, List<string>> linksUp = tree.Descendants().ToDictionary(descendant => descendant.Name.ToString(), descendant => descendant.Ancestors().Select(x => x.Name.ToString()).ToList());
+            Dictionary<string, List<string>> linksDown = tree.DescendantsAndSelf().Where(element => element.HasElements).ToDictionary(descendant => descendant.Name.ToString(), descendant => descendant.Ancestors().Select(x => x.Name.ToString()).ToList()); 
+        }
+    }
+}
---
+++ tree generator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace tree_generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 10; // максимальное число потомков
+            int h = 10; // высота
+            var random = new Random();
+            XElement tree =new XElement("tree");
+            
+            List<XElement> newlists= new List<XElement>() { tree };
+            for (int level = 0; level < h; level++)
+            {
+                List<XElement> lists = newlists;
+                newlists = new List<XElement>();
+                foreach (var node in lists)
+                {
+                    for (int i = 0; i < random.Next(k); i++)
+                    {
+                        var newList = new XElement(node.Name+"space"+i);
+                        newlists.Add(newList);
+                        node.Add(newList);
+                    }
+                }
+            }
+            tree.Save("../../../tree.xml");
+        }
+    }
+}
