a2108e82fcfd75d562a9973a80b409aa49220a15
Sergey303 <s@s.s>
2016-01-19 12:23:51 +0600

    parents search

--- Polar/ChildrenTree.cs
+++ Polar/ChildrenTree.cs
@@ -6,31 +6,59 @@ using PolarDB;
 
 namespace Polar
 {
-    class ChildrenTree
+    internal class ChildrenTree
     {
-        public  Dictionary<string, long> searchIndex;
-        public  PaCell paCell;
-        private Func<string, int> coding;
-        private Func<int, string> decoding;
-        private int height;
+        private Dictionary<string, long> searchIndex;
+        private readonly PaCell paCell;
+        private int maxChildrenCount;
+        private readonly Dictionary<string, int> coding;
+        private readonly Dictionary<int, string> decoding = new Dictionary<int, string>();
+        private readonly int height;
 
-        public ChildrenTree(Func<string, int> coding, Func<int, string> decoding, int height)
+        public ChildrenTree(XElement tree)
         {
-            this.coding = coding;
-            this.decoding = decoding;
-            this.height = height;
+            var rootCode = tree.Name.ToString();
+            height = GetTreeHeight(tree);
+            coding = new Dictionary<string, int> ();
+            GetMaxChiledrenCount(tree);
             PType treePType = Enumerable.Range(0, height).
-            Aggregate(new PType(PTypeEnumeration.integer),
-                (PType res, int level) =>
-                    new PTypeRecord(new NamedType("value", new PType(PTypeEnumeration.integer)),
-                        new NamedType("children", new PTypeSequence(res))));
+                Aggregate(new PType(PTypeEnumeration.integer),
+                    (PType res, int level) =>
+                        new PTypeRecord(new NamedType("value", new PType(PTypeEnumeration.integer)),
+                            new NamedType("children", new PTypeSequence(res))));
             paCell = new PaCell(treePType, "../../tree.pa", false);
+            CreateCoding(tree, 0);
         }
+
+        private void GetMaxChiledrenCount(XElement tree)
+        {
+            var count = tree.Elements().Count();
+            if (count >= maxChildrenCount)
+                maxChildrenCount = count + 1;
+            foreach (var child in tree.Elements())
+            {
+                GetMaxChiledrenCount(child);
+            }
+        }
+
+        private void CreateCoding(XElement tree, int rootCode)
+        {
+            var key = tree.Name.ToString();
+            coding.Add(key, rootCode);
+            decoding.Add(rootCode, key);
+
+            int i = 1;
+            foreach (var child in tree.Elements())
+            {
+                CreateCoding(child, rootCode*maxChildrenCount + (i++));
+            }
+        }
+
 
         public void ReCreate(XElement tree)
         {
             paCell.Clear();
-            var valu = CreateTree(tree, 0, height);
+            var valu = CreateTree(tree, 0);
             paCell.Fill(valu);
             searchIndex = new Dictionary<string, long>();
             AddInIndex(paCell.Root);
@@ -39,14 +67,14 @@ namespace Polar
         private void AddInIndex(PaEntry entry)
         {
             var index = (int) entry.Field(0).Get();
-            searchIndex.Add(decoding(index), entry.offset);
+            searchIndex.Add(decoding[index], entry.offset);
             foreach (var chiledEntry in entry.Field(1).Elements())
             {
                 AddInIndex(chiledEntry);
             }
         }
 
-        private  void Append(XElement tree)
+        private void Append(XElement tree)
         {
             if (tree.Elements().Any())
             {
@@ -63,21 +91,21 @@ namespace Polar
             }
         }
 
-        public  object CreateTree(XElement x, int level, int height)
+        public object CreateTree(XElement x, int level)
         {
 
             return level != height
                 ? (object)
                     new object[]
-                    {Program.Coding(x.Name.ToString()), x.Elements().Select(c => CreateTree(c, level + 1, height)).ToArray()}
-                : Program.Coding(x.Name.ToString());
+                    {coding[x.Name.ToString()], x.Elements().Select(c => CreateTree(c, level + 1)).ToArray()}
+                : coding[x.Name.ToString()];
         }
 
-        private  void CreateTree(XElement x, PaEntry paTreeNodeCollection)
+        private void CreateTree(XElement x, PaEntry paTreeNodeCollection)
         {
             if (x.Elements().Any())
             {
-                paTreeNodeCollection.AppendElement(new object[] { coding(x.Name.ToString()), new object[0]});
+                paTreeNodeCollection.AppendElement(new object[] {coding[x.Name.ToString()], new object[0]});
                 var childNodeCollection = paTreeNodeCollection.Field(1);
                 foreach (var child in x.Elements())
                 {
@@ -86,19 +114,63 @@ namespace Polar
             }
             else
             {
-                paTreeNodeCollection.AppendElement((object)coding(x.Name.ToString()));
+                paTreeNodeCollection.AppendElement((object) coding[x.Name.ToString()]);
             }
         }
+        private static int GetTreeHeight(XElement x) => x.Elements().Any()
+           ? x.Elements().Max(x1 => GetTreeHeight(x1) + 1)
+           : 1;
 
-        
-        public T[] GetChildren<T>(string name, Func<int, T> decode )
+        public string[] GetChildren(string name)
         {
             if (paCell.Root.Type.Vid == PTypeEnumeration.integer)
-                return new T[0];
+                return new string[0];
             var paEntry = paCell.Root;
             paEntry.offset = searchIndex[name];
-            return paEntry.Field(1).Elements().Select(entry => (int)entry.Field(0).Get())
-                .Select(decode).ToArray();
+            return paEntry.Field(1).Elements().Select(entry => (int) entry.Field(0).Get())
+                .Select(c=>decoding[c]).ToArray();
+        }
+
+       
+
+
+        public bool TestConnection(string node1, string node2)
+        {
+            int code1, code2, div;
+            if (!coding.TryGetValue(node1, out code1)) return false;
+            if (!coding.TryGetValue(node2, out code2)) return false;
+            if (code2 > code1)
+            {
+                div = code2;
+                return
+                    Enumerable.Range(1, height)
+                        .Any(i =>
+                        {
+                            div = div / maxChildrenCount;
+                            return code1 == div;
+                        });
+            }
+            else
+                div = code1;
+            return
+                Enumerable.Range(1, height)
+                    .Any(i =>
+                    {
+                        div = div / maxChildrenCount;
+                        return code2 == div;
+                    });
+        }
+
+        public IEnumerable<string> GetParents(string node)
+        {
+            int code;
+            if (!coding.TryGetValue(node, out code))
+                yield break;
+            while (code > 0)
+            {
+                code = code / maxChildrenCount;
+                yield return decoding[code];
+            }
         }
     }
 }
--- Polar/Program.cs
+++ Polar/Program.cs
@@ -11,48 +11,36 @@ namespace Polar
     class Program
     {
 
-        static List<string> names=new List<string>();
 
         static void Main(string[] args)
         {
             var tree = XElement.Load("../../../tree.xml");
-            int height = GetTreeHeight(tree);
         
-            ChildrenTree tree4SeaChildren=new ChildrenTree(Coding, i => names[i], height);
+        
+            ChildrenTree treeObjects=new ChildrenTree(tree);
            
+            treeObjects.ReCreate(tree);
             
             //Append(tree, paCell);
            
 
-            var children = tree4SeaChildren.GetChildren("tree", i=> names[i]);
-            children = tree4SeaChildren.GetChildren("treespace0", i => names[i]);
+            var children = treeObjects.GetChildren("tree");
+            children = treeObjects.GetChildren("treespace1");
 
 
+            var parents = treeObjects.GetParents(children[0]).ToArray();
+            var testConnection = treeObjects.TestConnection(children[children.Length - 1], children[children.Length - 2]);
+            testConnection = treeObjects.TestConnection(parents[0], children[children.Length - 2]);
+            testConnection = treeObjects.TestConnection(parents[1], children[children.Length - 2]);
+            testConnection = treeObjects.TestConnection("tree", children[children.Length - 2]);
 
-            PType parentsSearchType=new PTypeSequence(new PType(PTypeEnumeration.integer));
-            PaCell parentSequence=new PaCell(parentsSearchType, "../../parentsSeq.pa", false);
-            parentSequence.Clear();
-            parentSequence.Fill(new object[0]);
-            foreach (var node in tree.Elements())
-            {
-                foreach (var rows in parentSequence.Root.Elements())
-                {
-                    
-                }
-            }
         }
 
         #region Create
 
-        private static int GetTreeHeight(XElement x) => x.Elements().Any()
-            ? x.Elements().Max(x1 => Program.GetTreeHeight(x1) + 1)
-            : 1;
+    
 
-        public static int Coding(string s)
-        {
-            names.Add(s);
-            return names.Count - 1;
-        }
+   
 
         #endregion
         
